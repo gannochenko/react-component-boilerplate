e2348f3f8d712797d1c1db11957f0ee43a374196
Sergei <gannochenko.sv@gmail.com>
2020-12-28 23:15:59 +0100

    fix

--- component/template/[component_name_pascal]/hooks/use[component_name_pascal]ClassNames.ts
+++ component/template/[component_name_pascal]/hooks/use[component_name_pascal]ClassNames.ts
@@ -3,11 +3,11 @@ import { makeClassName } from '../../../util/makeClassName';
 
 export const use<%- component_name_pascal %>ClassNames = (
     componentName: string,
-    className: string
+    className?: string
 ) =>
     useMemo(
         () => ({
-            root: `${className} ${makeClassName(componentName)}`,
+            root: `${className || ''} ${makeClassName(componentName)}`.trim(),
             innerNode: makeClassName(componentName, "InnerNode"),
         }),
         [componentName, className]
